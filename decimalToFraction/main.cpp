15dc7bfc8e2490db6c1f0dff98edaeb6ca60e46d
kswain1 <kehlinswain@yahoo.com>
2014-10-27 18:17:20 -0400

    Initial Commit

---
+++ decimalToFraction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  decimalToFraction
+//
+//  Created by kehlin swain on 10/27/14.
+//  Copyright (c) 2014 Kehlin Swain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
